7c568423346ffa28d392f47527ac9a8eb2a6fa77
shatore22 <you@example.com>
2024-12-05 09:41:09 +0100

    nwm

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
   function getRandomInt(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
   }
   
   const voteButton = () => {
