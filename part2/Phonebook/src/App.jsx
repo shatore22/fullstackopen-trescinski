3bfd69dfe8571a54017279bc70086bef9f8bb9d7
shatore22 <you@example.com>
2025-01-27 11:49:20 +0100

    postep

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -1,34 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Person from './components/Person'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = (props) => {
+  const [persons, setPersons] = useState(props.persons || []) 
+  const [newPerson, setNewPerson] = useState('a new person...')
+
+  const handlePersonChange = (event) => {    
+    console.log(event.target.value)    
+    setNewPerson(event.target.value)  
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      content: newPerson,
+      id: String(persons.length + 1),
+    }
+    
+    persons.forEach(person => {
+      if(newPerson == person){
+        alert(`${newPerson} is already added to phonebook`)
+      }
+    });
+
+    setPersons(persons.concat(personObject))
+    setNewPerson('')
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <div>
+      <h2>Phonebook</h2>
+        <div>
+          <form onSubmit={addPerson}>        
+            name: <input value={newPerson} onChange={handlePersonChange}/>   
+            <button type="submit">save</button>      
+          </form>  
+        </div>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <Person key={person.id} person={person} />
+        )}
+      </ul>
+    </div>
   )
 }
 
